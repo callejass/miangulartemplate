182046f8e4557a585aeb86235864b6eba74f77f0
Loli-momo <lolicall.lc@gmail.com>
2023-05-24 10:51:54 +0200

    con getnombre y provincias

--- src/app/features/account/detail-account/detail-account.component.ts
+++ src/app/features/account/detail-account/detail-account.component.ts
@@ -9,21 +9,40 @@ import { TablasMaestrasService } from 'src/app/core/services/tablas-maestras.ser
   styleUrls: ['./detail-account.component.css']
 })
 export class DetailAccountComponent implements OnInit {
-  miUsuario!: User|null;
+  miUsuario: User|null=null;
   listaRoles: Rol[] = [];
 listaProvincias: Provincia[] = [];
   constructor(private authService:MiAuthService,
     private tablasMaestras: TablasMaestrasService ) { }
 
   ngOnInit(): void {
-    this.miUsuario=this.authService.miGetUsuario();
     
+    this.miUsuario=this.authService.miGetUsuario()!;
+    this.tablasMaestras.getData<Rol>('roles').subscribe((roles)=>{
+      this.listaRoles=roles;
+    });
+    this.tablasMaestras.getData<Provincia>('provincias').subscribe((provincia)=>{
+      this.listaProvincias=provincia
+    })
   }
-  getNombreRoles(codigosRoles: string[]): string[] {
+  getNombreProvincia(codigoProvincia: string | undefined): string {
+    if (!codigoProvincia) {
+      return "";
+    }
+  
+    const provincia = this.listaProvincias.find(p => p.codigo === codigoProvincia);
+    
+    return provincia ? provincia.nombre : "";
+  }
+  
+  getNombreRoles(codigosRoles: string[]|undefined): string[] {
+    if (!codigosRoles){
+      return []
+    } else{
     return codigosRoles.map((codigo) => {
       const rol = this.listaRoles.find((r) => r.codigo === codigo);
       return rol ? rol.nombre : "";
-    });
+    });}
   }
 
 }
